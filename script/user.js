c15d0c59ea85970baa8b7069d9a6be38f131bcac
Mariel Dunietz <mariel.dunietz@gmail.com>
2016-06-02 17:06:09 -0700

    Refine styles, add content, tweak user scripts.

--- script/user.js
+++ script/user.js
@@ -1,11 +1,12 @@
 $("document").ready(function () {
   'use strict';
   function ieSucks() {
-    var browserVersion = navigator.appName;
+    var browserVersion = navigator.appName, dialog = $("#popup");
     if (browserVersion === "Microsoft Internet Explorer") {
-      window.alert("You are using an unsupported browser. In order to best view this site, you should upgrade to the latest version of Internet Explorer, or use a different browser such as Safari, Chrome, or Firefox.");
+      // window.alert("You are using an unsupported browser. In order to best view this site, you should upgrade to the latest version of Internet Explorer, or use a different browser such as Safari, Chrome, or Firefox.");
+      dialog.popup("open");
     } else {
-      console.log("Good job!");
+      dialog.remove();
     }
   }
   ieSucks();
