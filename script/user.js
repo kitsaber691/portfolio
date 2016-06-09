e6e4badf271c61154f4c5b7c2ae708c3190692f3
Mariel Dunietz <mariel.dunietz@gmail.com>
2016-06-09 00:45:02 -0700

    Major style changes

--- script/user.js
+++ script/user.js
@@ -1,13 +1,12 @@
-$("document").ready(function () {
+function ieSucks() {
   'use strict';
-  function ieSucks() {
-    var browserVersion = navigator.appName, dialog = $("#popup");
-    if (browserVersion === "Microsoft Internet Explorer") {
-      // window.alert("You are using an unsupported browser. In order to best view this site, you should upgrade to the latest version of Internet Explorer, or use a different browser such as Safari, Chrome, or Firefox.");
-      dialog.popup("open");
-    } else {
-      dialog.remove();
-    }
+  var browserVersion = navigator.appName, dialog = $("#popup");
+  if (browserVersion === "Microsoft Internet Explorer") {
+    // window.alert("You are using an unsupported browser. In order to best view this site, you should upgrade to the latest version of Internet Explorer, or use a different browser such as Safari, Chrome, or Firefox.");
+    dialog.popup("open");
+  } else {
+    dialog.remove();
+    console.log("Thank you!");
   }
-  ieSucks();
-});
+}
+ieSucks();
